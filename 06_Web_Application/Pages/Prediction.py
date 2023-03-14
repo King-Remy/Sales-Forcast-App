ad72a98fa99fb1b74afc4bc372237d45b7971892
King Remy Igbokwe <king.igbokwe@gmail.com>
2023-03-14 21:06:43 +0000

    added event type encoded json file, added new input options (event type ) and restrictions

--- 06_Web_Application/Pages/Prediction.py
+++ 06_Web_Application/Pages/Prediction.py
@@ -1,13 +1,21 @@
 import streamlit as st
 import numpy as np
 import json
+import datetime
+from datetime import date, timedelta
 import pandas as pd
 import xgboost as xgb
+import os
 
 
 # Setup data from csv
 # df = pd.read_csv("C:\Users\KingRemy\OneDrive - University of Keele\Documents\Collaborative App Development\Coursework\Stored_dataset\client_219_153_EDA.csv", header=0, delimiter=',')
 
+# load encoded eventtype file
+config_path = os.path.abspath('..')
+
+with open(config_path + '\eventtype_encoder.json', 'r') as f:
+    config = json.load(f)
 
 #Caching the model for faster loading
 @st.cache
@@ -55,8 +63,13 @@ st.markdown("Using XGBoost, make predictions on the distribution of likely sales
             "The prediction will appear on the graphs and table below to intuit how the prediction was made.")
 st.sidebar.header("Make Prediction")
 
-start_date = st.sidebar.text_input("Event Start Date")
-weeks_to_event = st.sidebar.text_input("Promotion Start (Weeks to Event)")
+# Creating defaul date for event start date input
+today =date.today()
+
+# Creating inputs and button
+event_type = st.selectbox("Event Type:", config.keys() )
+start_date = st.sidebar.date_input("Event Start Date", today)
+weeks_to_event = st.sidebar.number_input("Promotion Start (Weeks to Event)", min_value=0, max_value=100, value=1)
 make_pred = st.sidebar.button("Predict")
 
 # Managing input data
@@ -65,7 +78,7 @@ p1 = ""
 # Making prediction and displaying data
 if make_pred:
     p1 = pd.to_datetime(p1)         # Converting startdate input into datetime
-    purchase_period_prediction = predict_period(p1)
+    purchase_period_prediction = predict_period(start_date)
     
     st.text_area("Predicted purchase period", value=purchase_period_prediction,height=40)
     # st.subheader(f"Predicted Species: {species_pred}")
