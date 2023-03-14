710c54153b6e60fc068bd7370ebd9f17b71dfe94
King Remy Igbokwe <king.igbokwe@gmail.com>
2023-03-14 15:41:37 +0000

    removed wrong variable in make_pre.py for streamlit web app

--- 06_Web_Application/make_pred.py
+++ 06_Web_Application/make_pred.py
@@ -42,7 +42,7 @@ def predict_period(StartDate):
     StartYear = StartDate.year
     StartDayofMonth = StartDate.day
     StartWeekofYear = StartDate.weekofyear
-    prediction_out = model_period.predict(pd.DataFrame([[Season,StartHour,StartDayofWeek,StartQuarter, StartDayofyear, StartMonth, StartYear,StartDayofMonth,StartWeekofYear]], columns=[Season,Num_of_ticket,StartHour,StartDayofWeek,StartQuarter, StartDayofyear, StartMonth, StartYear,StartDayofMonth,StartWeekofYear]))
+    prediction_out = model_period.predict(pd.DataFrame([[Season,StartHour,StartDayofWeek,StartQuarter, StartDayofyear, StartMonth, StartYear,StartDayofMonth,StartWeekofYear]], columns=[Season,StartHour,StartDayofWeek,StartQuarter, StartDayofyear, StartMonth, StartYear,StartDayofMonth,StartWeekofYear]))
     # df['Purchase_period_Predicted'] = prediction_out
     return prediction_out
 
