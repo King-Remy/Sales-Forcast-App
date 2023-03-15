982c3b760a157f0846a65c75ed7ca65be5e808e0
King Remy Igbokwe <king.igbokwe@gmail.com>
2023-03-15 14:12:37 +0000

    updated columns to string for headers into dataframe for prediction function in prediction.py

--- 06_Web_Application/Pages/Prediction.py
+++ 06_Web_Application/Pages/Prediction.py
@@ -52,7 +52,9 @@ def predict_period(StartDate):
     StartYear = StartDate.year
     StartDayofMonth = StartDate.day
     StartWeekofYear = StartDate.weekofyear
-    prediction_out = model_period.predict(pd.DataFrame([[StartDayofyear, StartYear, StartDayofWeek, Season, StartQuarter, StartDayofMonth, StartWeekofYear, StartMonth, StartHour]], columns=[StartDayofyear, StartYear, StartDayofWeek, Season, StartQuarter, StartDayofMonth, StartWeekofYear, StartMonth, StartHour]))
+    # df = 
+    # st.table()
+    prediction_out = model_period.predict(pd.DataFrame([[StartDayofyear, StartYear, StartDayofWeek, Season, StartQuarter, StartDayofMonth, StartWeekofYear, StartMonth, StartHour]], columns=['StartDayofyear', 'StartYear', 'StartDayofWeek', 'Season', 'StartQuarter', 'StartDayofMonth', 'StartWeekofYear', 'StartMonth', 'StartHour']))
     # df['Purchase_period_Predicted'] = prediction_out
     return prediction_out
 
