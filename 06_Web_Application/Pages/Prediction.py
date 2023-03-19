5287ccf249ce85c3e8f2a1d3ae79df2144a2aeef
King Remy Igbokwe <king.igbokwe@gmail.com>
2023-03-19 12:55:44 +0000

    Update Prediction.py

--- 06_Web_Application/Pages/Prediction.py
+++ 06_Web_Application/Pages/Prediction.py
@@ -120,7 +120,7 @@ if make_pred:
     # conv = str(start_datetime)
     p1 = pd.to_datetime(start_datetime)
     Client = pd.DataFrame.from_dict([{"StartDate": p1}])
-    Client["StartDate"] = pd.to_datetime(Client["StartDate"])                # converting created Event Startdate column with users StartDate to datetime format
+    Client["StartDate"] = pd.to_datetime(Client["StartDate"],errors='coerce', utc=True)                # converting created Event Startdate column with users StartDate to datetime format
 
     purchase_period_prediction = predict_period(Client)
     
