7f4f9a15616f7e57fc83da5dec5c3b7a3e1398bb
King Remy Igbokwe <king.igbokwe@gmail.com>
2023-03-19 16:35:38 +0000

    Update Prediction.py

--- 06_Web_Application/Pages/Prediction.py
+++ 06_Web_Application/Pages/Prediction.py
@@ -97,9 +97,8 @@ st.sidebar.header("Make Prediction")
 # Creating inputs and button
 event_type = st.sidebar.selectbox("Event Type:", config.keys() )
 start_date = st.sidebar.date_input("Event Start Date", datetime.date.today())
-start_time = st.sidebar.time_input('Enter start time', datetime.time(0, 00))
 
-start_datetime = datetime.datetime.combine(start_date, start_time)
+start_datetime = datetime.datetime.combine(start_date, st.sidebar.time_input('Enter start time', datetime.time(0, 00)))
 
 st.success(f"{start_datetime}")
 
